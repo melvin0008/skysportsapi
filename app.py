c5f4c3ed17038821da7c0eb8591120b3498f70a6
Melvin <melvin0008@gmail.com>
2014-10-25 14:32:19 +0530

    added for teamnews

--- app.py
+++ app.py
@@ -15,27 +15,27 @@ app = Flask(__name__,static_url_path='')
 
 root_url = 'http://www1.skysports.com/'
 
-sports=["football","cricket","golf","rugbyleague","boxing","tennis","f1","darts","snooker","cycling","american-football","motor-racing","ice-hockey","baseball"]
+sports=["football","cricket","golf","rugby-league","boxing","tennis","f1","darts","snooker","cycling","american-football","motor-racing","ice-hockey","baseball"]
 
 @app.route('/sky/getlatest/v1.0/', methods=['GET'])
 def get_latest():
     index_url = root_url + "latest-news"+"/"	
     response = requests.get(index_url)
     latest={}
-
+    
     soup = bs4.BeautifulSoup(response.text)
     for sport in sports:
+        info=[]
     	for a in soup.select('div.site-wrapper a[href^=http://www1.skysports.com/'+sport+'/]'):
-    		latest[sport]= {"link":a.attrs.get('href'),"text":a.get_text()} 
+    		info.append({"link":a.attrs.get('href'),"text":a.get_text()}) 
+        latest[sport]=info
     return json.dumps(latest)
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/sky/getnews/<string:category>/v1.0/', methods=['GET'])
-def get_news(category):
-    index_url=root_url+ category +"/news/"
+def get_news(index_url):
     response = requests.get(index_url)
     latest=[]
     item={}
@@ -51,7 +51,23 @@ def get_news(category):
         #latest.append(itemdetail.h4.a.attrs.get('href'))
         item={"imgsrc":news.noscript.img.attrs.get('src'),"title":re.sub('\s+',' ',itemdetail.h4.get_text()),"shortdesc":itemdetail.p.get_text(),"link":itemdetail.h4.a.attrs.get('href')}
         latest.append(item)
-    return json.dumps(latest)
+    return latest
+
+@app.route('/sky/getnews/<string:category>/v1.0/', methods=['GET'])
+def get_sportsnews(category):
+    index_url=root_url+ category +"/news/"
+    latest=[]
+    latest=get_news(index_url)
+    return json.dumps(latest)    
+
+
+@app.route('/sky/<string:sport>/getteamnews/<string:team>/v1.0/', methods=['GET'])
+def get_teamsnews(sport,team):
+    index_url=root_url+ sport+"/teams/"+team+"/news/"
+    latest=[]
+    latest=get_news(index_url)
+    return json.dumps(latest) 
+
 
 
 @app.errorhandler(404)
